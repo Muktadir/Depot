dfab10dc2dda774a8f5d22d4f1608fe1f618c394
Muktadir <muktadir00@gmail.com>
2014-09-23 00:05:58 +0600

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+  validates :title, :presence => true, :uniqueness => true
+  validates :description, :presence => true
+
+  # validates :image_url, :presence => true, :format => {
+  #     :with => %r{\a \.(gif|png|jpg)$}i,
+  #     :message => 'Must be a URL for GIF, PNG or JPG file.'
+  # }
+  validates :price, :presence => true, :numericality => {:greater_than_or_equal_to => 0.01}
+
 end
